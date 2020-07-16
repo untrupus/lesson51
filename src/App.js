098387f966919963e01d88d64359da09bdf68185
Ivan Volodartsev <untrupus@gmail.com>
2020-07-16 13:50:58 +0600

    create app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Circle from './Circle/Circle';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    nums: [],
+  };
+
+  newNumbers = () => {
+    const numbers = [];
+    const fiveNum = [];
+    for (let i = 1; i < 37; i++) {
+      numbers.push(i)
+    }
+
+    const shuffle = (arr) => {
+      arr.sort(() => Math.random() - 0.5);
+    }
+
+    shuffle(numbers);
+
+    for (let n = 0; n < 5; n++) {
+      fiveNum.push(numbers[n]);
+    }
+
+    fiveNum.sort((a, b) => {
+      return a - b;
+    })
+
+    this.setState({
+      nums: fiveNum,
+    })
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <div className="rounds" id="rounds">
+            <Circle value={this.state.nums[0]}/>
+            <Circle value={this.state.nums[1]}/>
+            <Circle value={this.state.nums[2]}/>
+            <Circle value={this.state.nums[3]}/>
+            <Circle value={this.state.nums[4]}/>
+          </div>
+          <div>
+            <button onClick={this.newNumbers} className="btn">Try again</button>
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
+
+
